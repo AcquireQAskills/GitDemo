191ed5064d80d9ccf105186a3e7368ee99a5b32f
acquireqaskill@gmail.com <SrinivasaRao.Chowdary@ryan.com>
2019-08-11 12:09:40 +0530

    first program comit to git

---
+++ src/main/java/packagename/App.java
@@ -0,0 +1,13 @@
+package packagename;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/packagename/demomvnTest.java
@@ -0,0 +1,19 @@
+package packagename;
+
+import org.testng.annotations.Test;
+
+public class demomvnTest
+{
+	@Test
+	public void weblogin()
+	{
+		System.out.println("welcome to mvn weblogin");
+	}
+	@Test
+	public void mobileapp()
+	{
+
+		System.out.println("welcome to mvn mobileapp");
+	}
+
+}
